9b6c8dca4c47f8913de3b76bcce4954f0910b8ce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 21:24:58 +0000

    Deploy dev-launchers-sandbox/project__safe-keep to github.com/dev-launchers-sandbox/project__safe-keep.git:gh-pages

---
+++ precache-manifest.0c6914677fa8783363298a98407fe9dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a4f57f6614c28b51dc55d40819d720",
+    "url": "/dev/project__safe-keep/index.html"
+  },
+  {
+    "revision": "a40b23d13cd3779e402c",
+    "url": "/dev/project__safe-keep/static/css/main.3a6e24ca.chunk.css"
+  },
+  {
+    "revision": "bc5232aaad8856eaadc0",
+    "url": "/dev/project__safe-keep/static/js/2.aca3b584.chunk.js"
+  },
+  {
+    "revision": "a40b23d13cd3779e402c",
+    "url": "/dev/project__safe-keep/static/js/main.ff73b2bd.chunk.js"
+  },
+  {
+    "revision": "cc8f2b646d58ac2faf3f",
+    "url": "/dev/project__safe-keep/static/js/runtime~main.20df39b6.js"
+  }
+]);
